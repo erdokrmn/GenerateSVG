5cdbbe50217e03c9d3665ecd2cdd4da8917ff424
Erdinç Karaman <erdokrmn@hotmail.com>
2025-05-15 16:41:25 +0300

    erdokrmn

--- GenerateSVG/obj/Debug/net8.0/GenerateSVG.AssemblyInfo.cs
+++ GenerateSVG/obj/Debug/net8.0/GenerateSVG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GenerateSVG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc3263f77082b0c6eeda12b79b9a55dbd48f180")]
 [assembly: System.Reflection.AssemblyProductAttribute("GenerateSVG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GenerateSVG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
